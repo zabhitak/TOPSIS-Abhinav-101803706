8c27fdd3bbf321e902e83cddda22be9b200e8c68
zabhitak <zabhitak1292000@gmail.com>
2020-11-08 21:14:31 +0530

    topsis

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'TOPSIS-Abhinav-101803706',         # How you named your package folder (MyLib)
   packages = ['TOPSIS-Abhinav-101803706'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package for python implemetation of TOPSIS method for multiple criteria decision making',   # Give a short description about your library
   author = 'Abhinav Goyal',                   # Type in your name
   author_email = 'zabhi1292@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/zabhitak/TOPSIS-Abhinav-101803706',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/zabhitak/TOPSIS-Abhinav-101803706/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/zabhitak/TOPSIS-Abhinav-101803706/archive/0.2.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
