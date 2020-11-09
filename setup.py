fc43b1a2450a3da49c8236b0af17116d13176cd9
zabhitak <zabhitak1292000@gmail.com>
2020-11-09 11:52:38 +0530

    topsis

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 setup(
   name = 'TOPSIS-Abhinav-101803706',        
   version = '0.11',    
   license='MIT',      
   description = 'A package for python implemetation of TOPSIS method for multiple criteria decision making', 
-  long_description=long_description,
-  long_description_content_type="text/markdown",
+  # long_description="long_description",
+  # long_description_content_type="text/markdown",
   author = 'Abhinav Goyal',                  
   author_email = 'zabhi1292@gmail.com',     
   url = 'https://github.com/zabhitak/TOPSIS-Abhinav-101803706',  
-  download_url = 'https://github.com/zabhitak/TOPSIS-Abhinav-101803706/archive/0.11.tar.gz',    
+  download_url = 'https://github.com/zabhitak/TOPSIS-Abhinav-101803706/archive/0.12.tar.gz',    
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
   install_requires=[          
           'numpy',
